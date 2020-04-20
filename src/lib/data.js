873094ff03a5ead1805e52f7b8b930fc9a24634a
funkyremi <remi.testa@gmail.com>
2020-04-20 19:10:08 +0200

    Remove unused code

--- src/lib/data.js
+++ src/lib/data.js
@@ -11,43 +11,36 @@ export const profileSchema = [
 export const reasons = [
   {
     shortText: "Travail",
-    text: `Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle, lorsqu'ils sont indispensables à l'exercice d’activités ne pouvant être organisées sous forme de télétravail ou déplacements professionnels ne pouvant être différés.`,
     position: ["x", 76, 527, 19],
     faIcon: 'building',
   },
   {
     shortText: "Courses",
-    text: `Déplacements pour effectuer des achats de fournitures nécessaires à l’activité professionnelle et des achats de première nécessité dans des établissements dont les activités demeurent autorisées (liste des commerces et établissements qui restent ouverts).`,
     position: ["x", 76, 478, 19],
     faIcon: 'shopping-cart',
   },
   {
     shortText: "Santé",
-    text: `Consultations et soins ne pouvant être assurés à distance et ne pouvant être différés ; consultations et soins des patients atteints d'une affection de longue durée.`,
     position: ["x", 76, 436, 19],
     faIcon: 'heartbeat',
   },
   {
     shortText: "Famille",
-    text: `Déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou la garde d’enfants.`,
     position: ["x", 76, 400, 19],
     faIcon: 'users',
   },
   {
     shortText: "Sport",
-    text: `Déplacements brefs, dans la limite d'une heure quotidienne et dans un rayon maximal d'un kilomètre autour du domicile, liés soit à l'activité physique individuelle des personnes, à l'exclusion de toute pratique sportive collective et de toute proximité avec d'autres personnes, soit à la promenade avec les seules personnes regroupées dans un même domicile, soit aux besoins des animaux de compagnie.`,
     position: ["x", 76, 345, 19],
     faIcon: 'running',
   },
   {
     shortText: "Judiciaire",
-    text: `Convocation judiciaire ou administrative.`,
     position: ["x", 76, 298, 19],
     faIcon: 'balance-scale',
   },
   {
     shortText: "Mission",
-    text: `Participation à des missions d’intérêt général sur demande de l’autorité administrative.`,
     position: ["x", 76, 260, 19],
     faIcon: 'hands-helping',
   }
