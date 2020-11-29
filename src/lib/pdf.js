433ed6a83b3ea407a4632734f52e69961eb88134
funkyremi <remi.testa@gmail.com>
2020-11-29 18:43:47 +0100

    Mise a jour de l'attestation

--- src/lib/pdf.js
+++ src/lib/pdf.js
@@ -67,21 +67,21 @@ export async function generatePdf(profile, settings) {
 
   const page1 = pdfDoc.getPages()[0];
 
-  drawText(page1, font, `${prenom} ${nom}`, 119, 696);
-  drawText(page1, font, dateDeNaissance, 119, 674);
-  drawText(page1, font, lieuDeNaissance, 297, 674);
-  drawText(page1, font, `${addresse} ${codePostal} ${ville}`, 133, 652);
+  drawText(page1, font, `${prenom} ${nom}`, 107, 657);
+  drawText(page1, font, dateDeNaissance, 107, 627);
+  drawText(page1, font, lieuDeNaissance, 240, 627);
+  drawText(page1, font, `${addresse} ${codePostal} ${ville}`, 124, 596);
 
   drawText(
     page1,
     font,
     profile.ville,
-    105,
-    177,
+    93,
+    122,
     idealFontSize(font, profile.ville, 83, 7, 11) || 7
   );
-  drawText(page1, font, `${creationDate}`, 91, 153, 11);
-  drawText(page1, font, `${creationHour}`, 264, 153, 11);
+  drawText(page1, font, `${creationDate}`, 76, 92, 11);
+  drawText(page1, font, `${creationHour}`, 246, 92, 11);
 
   drawText(page1, font, ...settings.selectedReason.position);
 
@@ -93,32 +93,35 @@ export async function generatePdf(profile, settings) {
     `Adresse: ${addresse} ${codePostal} ${ville}`,
     `Sortie: ${creationDate} a ${creationHour}`,
     `Motif: ${settings.selectedReason.code}`,
-  ].join("; ");
+    "",
+  ].join(";\n");
+  const qrTitle1 = 'QR-code contenant les informations ';
+  const qrTitle2 = 'de votre attestation numérique';
   const generatedQR = await generateQR(dataForQrCode);
   const qrImage = await pdfDoc.embedPng(generatedQR);
+  page1.drawText(qrTitle1 + '\n' + qrTitle2, { x: 415, y: 135, size: 9, font, lineHeight: 10 })
   page1.drawImage(qrImage, {
     x: page1.getWidth() - 156,
-    y: 100,
+    y: 25,
     width: 92,
     height: 92,
   });
-
   if (profile.signature) {
     const signature = await pdfDoc.embedPng(profile.signature);
     page1.drawImage(signature, {
       x: 120,
-      y: 90,
+      y: 20,
       width: 100,
       height: 40,
     });
   }
-
   pdfDoc.addPage();
 
   const page2 = pdfDoc.getPages()[1];
+  drawText(page2, font, qrTitle1 + qrTitle2, 50, page2.getHeight() - 70, 11);
   page2.drawImage(qrImage, {
     x: 50,
-    y: page2.getHeight() - 350,
+    y: page2.getHeight() - 390,
     width: 300,
     height: 300,
   });
